6fc65b55d14b2ade3e20b7047079db3aea883dad
hansy2577B <hansyngampaka@gmail.com>
2026-03-01 15:07:05 +0100

    Update and rename main.js to Script/MakeLocalAnim.js

--- Script/MakeLocalAnim.js
+++ Script/MakeLocalAnim.js
@@ -1,24 +1,31 @@
-function makeLocalAnim(name,animScr,L) {
-  for (var i = 0; i < L + 1; i++) {
-    var thing = document.createElement("img");
+// script create an animation using only inserted images. 
+
+function makeLocalAnim(name,animScr,animLength) {
+  for (var i = 0; i < animLength + 1; i++) {
+    var thing = document.createElement("img"); // or document.getElementById(custom document id here).createElement("img");
+    //                                   ↑ it can be a ("a") for text
     document.body.appendChild(thing);
-    
-    thing.style.position = "absolute";
-    thing.style.visibility = "hidden"
+
+    // custom setting
+    thing.style.position = "absolute"; // or if you want a precise position ⟨ .left = "0px"; ⟩
     thing.style.zindex = "0";
-    thing.src = animScr + i + ".png";
     thing.style.backgroundColor = "black";
+
+    // do not modify 
     thing.id = name + i;
+    thing.style.visibility = "hidden";
+    thing.src = animScr + i + ".png";
+    
     console.log(name + i + " | "+L+" create")
   }
 }
 
-function playLocalAnim(name,L,speed) {
+function playLocalAnim(name,animLength,speed) {
   var localAnimValue = {
     i: 0,
     i2: 0,
     i3:0,
-    L2: L + 1
+    L2: animLength + 1
   };
   
   setInterval(() => {
@@ -37,28 +44,21 @@ function playLocalAnim(name,L,speed) {
       localAnimValue.i2 = 0;
       localAnimValue.i ++;
       localAnimValue.i3 = localAnimValue.i + 1;
-
-      
     }
     
     
-    if (localAnimValue.i >= L) {
+    if (localAnimValue.i >= animLength) {
       localAnimValue.i = 0;
       localAnimValue.i2 = 0;
       localAnimValue.i3 = 1;
       document.getElementById(name + localAnimValue.i).style.visibility = "visible";
     } else {
-      console.log("j")
+      //console.log("j")
       document.getElementById(name + localAnimValue.i).style.visibility = "visible";
     }
-    console.log(localAnimValue.i,document.getElementById(name + localAnimValue.i).style.zindex)
-
+    //console.log(localAnimValue.i,document.getElementById(name + localAnimValue.i).style.zindex)
   },speed)
 }
 
-makeLocalAnim("yo","M",4)
-
-document.onclick = function () {
-  playLocalAnim("yo",5,100)
-}
-// omg it fanilly work yaaaaz !!!!!!!!
+makeLocalAnim("test","myImage",4); 
+playLocalAnim("test",5,100)
